52c652bbf590bf116c42514d0d08b184066880fe
Tangles <tim.eastwood@hotmail.com>
2022-12-28 14:21:32 +1100

    check for wrong fandoms

--- handleAo3Url.py
+++ handleAo3Url.py
@@ -6,7 +6,8 @@ from emoji import emojiLookup
 
 
 async def handleAo3Url(message):
-    url = 'https://archiveofourown.org/' + find_between(message.content + ' ', 'archiveofourown.org/', ' ')
+    url = 'https://archiveofourown.org/' + \
+        find_between(message.content + ' ', 'archiveofourown.org/', ' ')
     workId = urlToWorkId(url)
     print('Fetching story id ' + workId + ' from AO3...')
     if hasWorkBeenPosted(workId):
@@ -15,42 +16,48 @@ async def handleAo3Url(message):
     else:
         story = parseStory(url, workId)
 
+        if story['fandom'] != 'The Locked Tomb Series | Gideon the Ninth Series - Tamsyn Muir':
+            print('Work belonged to another fandom, skipping.')
+            return False
+
         def emoji(category, name):
             emojiName = emojiLookup[category][name]
             return str(discord.utils.get(message.guild.emojis, name=emojiName))
-        emojiString = emoji('rating', story['rating']) + '' + emoji('category', story['category']) + '\n' + emoji('warnings', story['warnings']) + '' + emoji('iswip', story['isWIP'])
+        emojiString = emoji('rating', story['rating']) + '' + emoji('category', story['category']) + '\n' + emoji(
+            'warnings', story['warnings']) + '' + emoji('iswip', story['isWIP'])
         embed = discord.Embed(
-            color = discord.Color.purple(),
-            title = story['fullTitle'],
-            description = story['fandom'],
-            url = url,
-            type = 'rich'
+            color=discord.Color.purple(),
+            title=story['fullTitle'],
+            description=story['fandom'],
+            url=url,
+            type='rich'
         )
         embed.set_author(
-            name = story['author'],
-            url = story['authorWorksUrl'],
-            icon_url = story['authorIconUrl']
+            name=story['author'],
+            url=story['authorWorksUrl'],
+            icon_url=story['authorIconUrl']
         )
-        embed.set_thumbnail(url = 'https://archiveofourown.org/images/ao3_logos/logo_42.png')
+        embed.set_thumbnail(
+            url='https://archiveofourown.org/images/ao3_logos/logo_42.png')
         embed.add_field(
-            name = story['summaryTruncated'],
-            value = emojiString + '\n\n' + 'Shared by ' + message.author.mention,
-            inline = False
-        )
-        embed.add_field(
-            name = 'Words',
-            value = story['wordCount'], 
-            inline = True
+            name=story['summaryTruncated'],
+            value=emojiString + '\n\n' + 'Shared by ' + message.author.mention,
+            inline=False
         )
         embed.add_field(
-            name = 'Chapters',
-            value = story['chapterCount'] + '/' + story['chapterCountTotal'], 
-            inline = True
+            name='Words',
+            value=story['wordCount'],
+            inline=True
         )
         embed.add_field(
-            name = 'Kudos',
-            value = story['kudos'], 
-            inline = True
+            name='Chapters',
+            value=story['chapterCount'] + '/' + story['chapterCountTotal'],
+            inline=True
+        )
+        embed.add_field(
+            name='Kudos',
+            value=story['kudos'],
+            inline=True
         )
 
         if False:
@@ -58,24 +65,21 @@ async def handleAo3Url(message):
             for tag in story['tags']:
                 if tag['tagCategory'] != lastTagCat:
                     embed.add_field(
-                        name = '\u200b',
-                        value = '\u200b', 
-                        inline = False
+                        name='\u200b',
+                        value='\u200b',
+                        inline=False
                     )
                     lastTagCat = tag['tagCategory']
                 embed.add_field(
-                    name = tag['tagCategory'],
-                    value = tag['tagLabel'],
-                    inline = True
+                    name=tag['tagCategory'],
+                    value=tag['tagLabel'],
+                    inline=True
                 )
         embed.set_footer(
-            text = 'Fic last updated: ' + story['lastUpdated'] + ' | Bot trouble? Hit up Tangles#6014 | WorkID: ' + story['workId']
+            text='Fic last updated: ' +
+            story['lastUpdated'] +
+            ' | Bot trouble? Hit up Tangles#6014 | WorkID: ' + story['workId']
         )
         print('Adding ' + workId + ' to already-posted list.')
         hasWorkBeenPosted(urlToWorkId(url), True)
         return embed
-        #exit()
-
-#def letsGo():
-    #handleAo3Url('this works quite nicely I think:https://archiveofourown.org/works/41537028/chapters/104176614 what do you think guys?')
-#letsGo()
