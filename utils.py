b7c61ef03566e61633317adf11331303bd59cef4
Tangles <tim.eastwood@hotmail.com>
2022-12-28 14:39:02 +1100

    add pathlib

--- utils.py
+++ utils.py
@@ -1,30 +1,37 @@
 from io import StringIO
 from html.parser import HTMLParser
+from pathlib import Path
+
 
 class MLStripper(HTMLParser):
     def __init__(self):
         super().__init__()
         self.reset()
         self.strict = False
-        self.convert_charrefs= True
+        self.convert_charrefs = True
         self.text = StringIO()
+
     def handle_data(self, d):
         self.text.write(d)
+
     def get_data(self):
         return self.text.getvalue()
+
 
 def strip_tags(html):
     s = MLStripper()
     s.feed(html)
     return s.get_data()
 
-def find_between( s, first, last ):
+
+def find_between(s, first, last):
     try:
-        start = s.index( first ) + len( first )
-        end = s.index( last, start )
+        start = s.index(first) + len(first)
+        end = s.index(last, start)
         return s[start:end]
     except ValueError:
         return ""
+
 
 def find_nth(haystack, needle, n):
     start = haystack.find(needle)
@@ -33,8 +40,9 @@ def find_nth(haystack, needle, n):
         n -= 1
     return start
 
-def hasWorkBeenPosted(workId, addToList = False):
-    file = open(r"workIDs.txt","r")
+
+def hasWorkBeenPosted(workId, addToList=False):
+    file = open(Path("workIDs.txt"), "r")
     lines = file.readlines()
     idDidExist = False
     for line in lines:
@@ -42,14 +50,15 @@ def hasWorkBeenPosted(workId, addToList = False):
             idDidExist = True
     file.close()
     if (addToList):
-        file2 = open(r"workIDs.txt","a")
+        file2 = open(Path("workIDs.txt"), "a")
         if not idDidExist:
             file2.write(workId + '\n')
         file2.close()
     return idDidExist
 
+
 def urlToWorkId(url):
-    urlPath = url.split('archiveofourown.org/',1)[1] + '/'
+    urlPath = url.split('archiveofourown.org/', 1)[1] + '/'
     result = urlPath[find_nth(urlPath, '/', 1)+1:find_nth(urlPath, '/', 2)]
     if '?' in result:
         return result.split('?')[0]
